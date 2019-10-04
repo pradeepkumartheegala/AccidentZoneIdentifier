85459625916c80d0e028d959c6d2de19ec148638
saikirandd <33051183+saikirandd@users.noreply.github.com>
2019-10-04 18:45:21 -0500

    added java code to buttons in main activity

--- app/src/main/java/com/example/accidentzoneidentifier/MainActivity.java
+++ app/src/main/java/com/example/accidentzoneidentifier/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.accidentzoneidentifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void loginBTN(View v){
+        Intent b1= new Intent(this,Login_Activity.class);
+        startActivity(b1);
+    }
+
+    public void signUpBTN(View v){
+        Intent b1= new Intent(this,Login_Activity.class);
+        startActivity(b1);
     }
 }
