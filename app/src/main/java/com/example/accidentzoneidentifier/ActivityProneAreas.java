4bbbca7fb55f9480b8613cfb1cd85e0406d2f203
Sunil Mundru <sunilmundru@gmail.com>
2019-10-04 18:37:44 -0500

    Added AccidentProneAreas Activity

---
+++ app/src/main/java/com/example/accidentzoneidentifier/ActivityProneAreas.java
@@ -0,0 +1,14 @@
+package com.example.accidentzoneidentifier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityProneAreas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prone_areas);
+    }
+}
