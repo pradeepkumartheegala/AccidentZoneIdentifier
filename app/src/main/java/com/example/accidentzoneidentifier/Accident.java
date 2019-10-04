0c8605c0771413f9119606263f62768c71e42ad5
SaiNikhilPippara <nikhil.pippara@gmail.com>
2019-10-04 16:41:25 -0500

    commit

---
+++ app/src/main/java/com/example/accidentzoneidentifier/Accident.java
@@ -0,0 +1,14 @@
+package com.example.accidentzoneidentifier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Accident extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accident);
+    }
+}
