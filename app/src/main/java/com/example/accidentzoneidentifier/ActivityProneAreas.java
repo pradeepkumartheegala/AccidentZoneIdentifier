d006988b7ae31ad717c5c5c9672efe6b29eda676
SaiNikhilPippara <nikhil.pippara@gmail.com>
2019-10-25 14:59:12 -0500

    updated accident prone areas by nikhil

--- app/src/main/java/com/example/accidentzoneidentifier/ActivityProneAreas.java
+++ app/src/main/java/com/example/accidentzoneidentifier/ActivityProneAreas.java
@@ -2,10 +2,14 @@ package com.example.accidentzoneidentifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityProneAreas extends AppCompatActivity {
 
@@ -13,10 +17,19 @@ public class ActivityProneAreas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prone_areas);
+
     }
     public void RouteBTN(View v){
+        SharedPreferences myData = getSharedPreferences("saveProne", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = myData.edit();
+        EditText start = findViewById(R.id.EditStart);
+        EditText dest = findViewById(R.id.editDest);
+       ed.putString("sta",start.getText().toString());
+        ed.putString("des",dest.getText().toString());
+        ed.commit();
+        Toast.makeText(getApplicationContext(),"ProneAreas Created",Toast.LENGTH_SHORT).show();
         Intent b1= new Intent(this,AccidentzoneMapsActivity.class);
-        Log.d("map","started");
         startActivity(b1);
+
     }
 }
