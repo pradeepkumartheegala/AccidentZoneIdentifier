fd6eea6e594a9a0f542611a7618adec9be622936
harshithgudpati <jinnu95@gmail.com>
2019-10-25 14:24:26 -0500

    Changes made to welcome page and login activity

--- app/src/main/java/com/example/accidentzoneidentifier/LoginWelcome.java
+++ app/src/main/java/com/example/accidentzoneidentifier/LoginWelcome.java
@@ -2,16 +2,28 @@ package com.example.accidentzoneidentifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginWelcome extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_welcome);
+        SharedPreferences myData = getSharedPreferences("save", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = myData.edit();
+        String  name = myData.getString("name","");
+        TextView displayTV = findViewById(R.id.displayUser);
+        displayTV.setText("Welcome "+name);
     }
     public void AccountBTN(View v){
         Intent b1= new Intent(this,Account.class);
