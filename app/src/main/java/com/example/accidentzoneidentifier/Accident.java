20d00cda7c6e920c469dca6ca154f5d17b9aea51
saikirandd <33051183+saikirandd@users.noreply.github.com>
2019-10-25 13:34:07 -0500

    added  data preferences for Accident by sai kiran Doddapaneni

--- app/src/main/java/com/example/accidentzoneidentifier/Accident.java
+++ app/src/main/java/com/example/accidentzoneidentifier/Accident.java
@@ -1,8 +1,11 @@
 package com.example.accidentzoneidentifier;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,5 +21,17 @@ public class Accident extends AppCompatActivity {
     public void reportBTN(View v) {
         Intent b1 = new Intent(this, LoginWelcome.class);
         startActivity(b1);
+        SharedPreferences myData = getSharedPreferences("saveAccident", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = myData.edit();
+        EditText location = findViewById(R.id.locationET);
+        EditText zipCode = findViewById(R.id.zipcodeET);
+        EditText street = findViewById(R.id.streetET);
+        EditText landmark = findViewById(R.id.landmarkET);
+        ed.putString("name", (location.getText().toString()));
+        ed.putString("phNo",zipCode.getText().toString());
+        ed.putString("address",street.getText().toString());
+        ed.putString("password",landmark.getText().toString());
+
+        ed.commit();
     }
 }
