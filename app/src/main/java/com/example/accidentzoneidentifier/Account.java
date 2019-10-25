963cebe1196e01f8befec63f9b72faea291d4466
sunilmundru <46757390+sunilmundru@users.noreply.github.com>
2019-10-25 14:31:58 -0500

    Account updating to perform functionality

--- app/src/main/java/com/example/accidentzoneidentifier/Account.java
+++ app/src/main/java/com/example/accidentzoneidentifier/Account.java
@@ -2,9 +2,12 @@ package com.example.accidentzoneidentifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Account extends AppCompatActivity {
 
@@ -12,11 +15,17 @@ public class Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        SharedPreferences myData = getSharedPreferences("save", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = myData.edit();
+        String name = myData.getString("name", "");
+        String password = myData.getString("password", "");
+        String email = myData.getString("email", "");
+        String phonenumber = myData.getString("phNo", "");
+        String address = myData.getString("address", "");
+
+
     }
 
 
-    public void saveBTN(View v){
-        Intent b1= new Intent(this,LoginWelcome.class);
-        startActivity(b1);
-    }
+
 }
