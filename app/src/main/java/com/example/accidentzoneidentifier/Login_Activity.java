a0ef1bec7d8589f01a08ed1c7f17169d5e827052
pradeepkumar <pradeepkumartheegala@gmail.com>
2019-10-25 13:51:09 -0500

    Changed the functionality of login activity to check user name with password

--- app/src/main/java/com/example/accidentzoneidentifier/Login_Activity.java
+++ app/src/main/java/com/example/accidentzoneidentifier/Login_Activity.java
@@ -1,11 +1,14 @@
 package com.example.accidentzoneidentifier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -14,11 +17,23 @@ public class Login_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
     }
-    public void LogWelcomeBTN(View v){
 
+    public void LogWelcomeBTN(View v) {
+        SharedPreferences myData = getSharedPreferences("save", Context.MODE_PRIVATE);
+        EditText userName = findViewById(R.id.loginEmail);
+        EditText userPwd = findViewById(R.id.loginPassword);
+        String email = myData.getString("email", "");
+        String pwd = myData.getString("password", "");
+        if (!email.equals("") && !pwd.equals("")) {
+            if (email.equalsIgnoreCase(userName.getText().toString()) && pwd.equalsIgnoreCase(userPwd.getText().toString())) {
+                Intent b1 = new Intent(this, LoginWelcome.class);
+                startActivity(b1);
+            } else {
+                Toast.makeText(getApplicationContext(), "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "NO DATA FOUND", Toast.LENGTH_SHORT).show();
 
-
-        Intent b1= new Intent(this,LoginWelcome.class);
-        startActivity(b1);
+        }
     }
 }
