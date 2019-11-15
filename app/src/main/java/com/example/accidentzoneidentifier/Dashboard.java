546afe765cd0efd0065e14e9be72c12e9ae4e5f1
saikirandd <33051183+saikirandd@users.noreply.github.com>
2019-11-15 12:52:44 -0600

    Updated dashboard java code and created dashboardData

--- app/src/main/java/com/example/accidentzoneidentifier/Dashboard.java
+++ app/src/main/java/com/example/accidentzoneidentifier/Dashboard.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
     }
     
     public void accidentBTN(View v){
